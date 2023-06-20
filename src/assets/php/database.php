963d62cc35ff87e6cd51e540aab36f5ed0e97b8b
Arnon2010 <vkoooo044@gmail.com>
2023-06-20 14:01:56 +0700

    commit 1

--- src/assets/php/database.php
+++ src/assets/php/database.php
@@ -5,14 +5,10 @@ header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Content-Type: application/json");
 
-// $db_host = '128.199.142.209';
-// $db_username = 'nang';
-// $db_password = 'QK4xD7&j2smFYY';
-// $db_name = 'ruts-map-wifi';
 
-$db_host = 'linuxdb2.rmutsv.ac.th';
+$db_host = 'localhost';
 $db_username = 'root';
-$db_password = ',miLiu;b=yp48';
+$db_password = '123456';
 $db_name = 'jumbomap_test';
 
 $mysqli = new mysqli($db_host, $db_username, $db_password, $db_name);
